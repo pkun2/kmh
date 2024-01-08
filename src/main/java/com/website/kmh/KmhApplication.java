72eca51c7914ae11090b1c9ff573d35a0f1deccf
pkun2 <pkun2@naver.com>
2024-01-08 20:12:14 +0900

    first commit

---
+++ src/main/java/com/website/kmh/KmhApplication.java
@@ -0,0 +1,13 @@
+package com.website.kmh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/website/kmh/KmhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.kmh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
