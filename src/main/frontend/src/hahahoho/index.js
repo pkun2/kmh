f7fb89b03eef2d51d6af92dccf46292caca469d4
janghoyeol <jhy9732@naver.com>
2024-01-10 15:57:22 +0900

    test

---
+++ src/main/frontend/src/hahahoho/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <p>안녕하세요</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
