394bc6647df60b7a4d4d33df9e3ff15bf34db127
janghoyeol <jhy9732@naver.com>
2024-01-11 17:30:44 +0900

    나는 돈이 없어요

--- src/main/frontend/src/pages/write/index.js
+++ src/main/frontend/src/pages/write/index.js
@@ -1,3 +1,36 @@
-import WritePage from "./writePage"
+import React, { Component } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import './index.css';
 
-export { WritePage }
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>제목</h2>
+                <CKEditor
+                    editor={ ClassicEditor }
+                    config={{
+                      placeholder: "내용을 입력하세요.",
+                  }}
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+            </div>
+        );
+    }
+}
+
+export default App;
