185b0f0a89432b4c52afc78b2cf0e41c28a32157
janghoyeol <jhy9732@naver.com>
2024-01-11 19:23:02 +0900

    코드는 맞게 작성한 것 같은디.. 8080 리다이렉션 오류로 안되는듯..

---
+++ src/main/java/com/website/kmh/repository/ChannelRepository.java
@@ -0,0 +1,7 @@
+package com.website.kmh.repository;
+
+import com.website.kmh.domain.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelRepository extends JpaRepository<Channel, Long> {
+}
