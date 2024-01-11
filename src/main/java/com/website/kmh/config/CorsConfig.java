604e703685fa9b510dbd40c555f17ffbe88c413d
pkun2 <pkun2@naver.com>
2024-01-11 16:09:49 +0900

    cors 설정 보안정책 모두 허용

--- src/main/java/com/website/kmh/config/CorsConfig.java
+++ src/main/java/com/website/kmh/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // 허용할 오리진(도메인)
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // 허용할 HTTP 메서드
                 .allowCredentials(false) // 자격 증명(쿠키, 인증 등) 허용 여부
                 .maxAge(3600); // preflight 요청의 유효 기간 설정
     }
