0a6fc8e435abf85a33a460c5f358de91739697f7
pkun2 <pkun2@naver.com>
2024-01-11 18:02:26 +0900

    회원가입 구현 완료

--- src/main/java/com/website/kmh/controller/UserController.java
+++ src/main/java/com/website/kmh/controller/UserController.java
@@ -1,17 +1,12 @@
 package com.website.kmh.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.website.kmh.domain.User;
 import com.website.kmh.service.UserService;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class UserController {
@@ -21,19 +16,6 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
-    }
-
-    @GetMapping(value = "/register", produces = "application/json")
-    public ResponseEntity<Map<String, String>> getRegister() {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello register");
-        return ResponseEntity.ok(response);
-    }
-    @GetMapping(value = "/login", produces = "application/json")
-    public ResponseEntity<Map<String, String>> getLogin() {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello login");
-        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/register")
