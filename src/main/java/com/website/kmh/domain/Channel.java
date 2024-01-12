bad3a839a76d1a2a25915a891838878e466df2f4
janghoyeol <jhy9732@naver.com>
2024-01-12 15:39:54 +0900

    React-Spring-Mysql 연동 해 데이터 가져와 화면에 출력 성공!

--- src/main/java/com/website/kmh/domain/Channel.java
+++ src/main/java/com/website/kmh/domain/Channel.java
@@ -1,7 +1,11 @@
 package com.website.kmh.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity(name = "channels")
 public class Channel {
 
@@ -9,30 +13,6 @@ public class Channel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long channel_id;
-
-    public Long getChannel_id() {
-        return channel_id;
-    }
-
-    public void setChannel_id(Long channel_id) {
-        this.channel_id = channel_id;
-    }
-
-    public String getChannel_name() {
-        return channel_name;
-    }
-
-    public void setChannel_name(String channel_name) {
-        this.channel_name = channel_name;
-    }
-
-    public Long getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
-    }
 
     private String channel_name;
 
