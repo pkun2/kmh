87e2c43da3337b2ffcf4f86fc28a82fc567b6f52
minkinghub <kp141@naver.com>
2024-01-16 17:05:59 +0900

    임시 저장

--- src/main/frontend/src/pages/search/MainSearchPage.js
+++ src/main/frontend/src/pages/search/MainSearchPage.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { getData } from "../../services";
 
 const MainSearchPage = () => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search).get('searchInput');
     const [searchList, setSearchList] = useState('');
 
     const handleSearch = async (event) => {
@@ -21,15 +24,38 @@ const MainSearchPage = () => {
 
     return (
         <>
-            <div>
-                전체 채널 검색
+            <div style = {{
+                marginRight : 5,
+                marginLeft: 5,
+            }}
+            >
+                <div style = {{
+                        padding: 10,
+                        borderBottom : "2px solid #000099",
+                        fontWeight: "bold",
+                        fontSize: 18
+                    }}
+                >
+                    전체 채널 검색
+                </div>
+                <div style = {{
+
+                    }}
+                >
+                    조회순 드롭다운, 출력할 갯수 드롭다운, 글 쓰기 버튼
+                </div>
+                <div>
+                    <div>
+                        번호 태그 제목 닉네임 작성일 조회수 추천
+                    </div>
+                    <div>
+                        리스트
+                    </div>
+                </div>
+                <div>
+                    페이지
+                </div>
             </div>
-            <div>
-                조회순 드롭다운, 출력할 갯수 드롭다운, 글 쓰기 버튼
-            </div>
-            <button onClick = {handleSearch}>
-                get 보내기
-            </button>
         </>
     )
 }
