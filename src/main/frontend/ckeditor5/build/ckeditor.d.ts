131b1841a463d8e62f5e1e0ace0d29c7538ba1ce
janghoyeol <jhy9732@naver.com>
2024-01-18 17:07:06 +0900

    build 추가

---
+++ src/main/frontend/ckeditor5/build/ckeditor.d.ts
@@ -0,0 +1,31 @@
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { Autosave } from '@ckeditor/ckeditor5-autosave';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
+import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import { DataFilter } from '@ckeditor/ckeditor5-html-support';
+import { Image, ImageCaption, ImageInsert, ImageStyle, ImageToolbar, ImageUpload } from '@ckeditor/ckeditor5-image';
+import { Indent } from '@ckeditor/ckeditor5-indent';
+import { AutoLink, Link } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Mention } from '@ckeditor/ckeditor5-mention';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import { Undo } from '@ckeditor/ckeditor5-undo';
+import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
+declare class Editor extends ClassicEditor {
+    static builtinPlugins: (typeof Alignment | typeof AutoLink | typeof Autoformat | typeof Autosave | typeof BlockQuote | typeof Bold | typeof CloudServices | typeof DataFilter | typeof Essentials | typeof FindAndReplace | typeof FontBackgroundColor | typeof FontColor | typeof FontFamily | typeof FontSize | typeof Heading | typeof HorizontalLine | typeof Image | typeof ImageCaption | typeof ImageInsert | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof Italic | typeof Link | typeof List | typeof MediaEmbed | typeof Mention | typeof Paragraph | typeof PasteFromOffice | typeof Table | typeof TableToolbar | typeof TextTransformation | typeof Undo | typeof SimpleUploadAdapter)[];
+    static defaultConfig: EditorConfig;
+}
+export default Editor;
--- src/main/frontend/src/pages/post/PostPage.js
+++ src/main/frontend/src/pages/post/PostPage.js
@@ -1,79 +1,73 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import { getData } from "../../services";
-import { CommonButton, PageNameBox, SearchResultBox, SearchBox } from "../../components";
+import {CommonButton, PageNameBox, SearchResultBox} from "../../components";
 
 const PostPage = () => {
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search).get('searchInput');
     const [searchList, setSearchList] = useState([]);
-    const [channelInfo, setChannelInfo] = useState({});
+    const [channelInfo, setChannelInfo] = useState({
+        channel_id: 1,
+        channel_name: "test",
+        user_id: 3,
+    })
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
-            // 예제에서는 검색어를 items로 전달하고 있습니다. 필요에 따라 수정하세요.
-            const items = {
-                searchInput: searchParams,
-            };
-
-            const response = await getData(items, 'api/posts/latest');
-            setSearchList(response);
+            const response = await getData({}, 'api/posts/latest');
+            const data = response.map(item => ({
+                number: item.postId,
+                tag: item.categoryTag,
+                title: item.title,
+                nickname: item.nickname,
+                view: item.viewCount,
+                like: item.goodCount
+            }));
+            setSearchList(data);
         };
-
-        const fetchChannelInfo = async () => {
-            // 채널 정보 가져오는 API 호출
-            const channelResponse = await getData({}, 'api/channel/get');
-            setChannelInfo(channelResponse);
-        };
-
         fetchData();
-        fetchChannelInfo();
-    }, [searchParams]);
+    }, []);
 
     const handlePost = (index) => {
-        navigate(`/postdetail?post_id=${encodeURIComponent(searchList[index].number)}`);
-    };
+        navigate(`/postdetail?post_id=${encodeURIComponent(searchList[index].number)}`)
+    }
 
-    const handleSearch = async (searchInput) => {
-        // 검색어 입력 시 호출할 함수
-        const items = {
-            searchInput,
-        };
-
-        const response = await getData(items, 'api/posts/latest');
-        setSearchList(response);
-    };
+    const handleWrite = () => {
+        navigate('/write');
+    }
 
     return (
         <>
-            <div style={{
-                marginRight: 5,
+            <div style = {{
+                marginRight : 5,
                 marginLeft: 5,
-            }}>
+            }}
+            >
                 <PageNameBox
-                    items={{ title: `${channelInfo.channel_name} 채널` }}
-                    styles={{
+                    items = {{title: `${channelInfo.channel_name} 채널`}}
+                    styles = {{
                         padding: 8,
                         borderBottom: "2px solid #000099",
                         fontWeight: "bold",
                         fontSize: 18
                     }}
                 />
-                <div style={{
+                <div style = {{
                     display: "flex",
                     borderBottom: "2px solid #000099",
                     alignItems: "center",
                     justifyContent: "flex-end",
-                    paddingTop: 5,
-                    paddingBottom: 5,
-                }}>
+                    paddingTop : 5,
+                    paddingBottom : 5,
+                }}
+                >
                     <select style={{
                         borderRadius: 0,
                         padding: 2,
                         marginLeft: 3,
                         outline: "none"
-                    }}>
+                    }}
+                    >
                         <option>조회순</option>
                     </select>
                     <select style={{
@@ -81,32 +75,29 @@ const PostPage = () => {
                         padding: 2,
                         marginLeft: 3,
                         outline: "none"
-                    }}>
+                    }}
+                    >
                         <option>30개</option>
                     </select>
                     <CommonButton
-                        styles={{ border: "1px solid gray", backgroundColor: "white", marginLeft: 3 }}
-                        items={{ title: "글 쓰기" }}
+                        styles={{border: "1px solid gray", backgroundColor: "white", marginLeft: 3}}
+                        items={{title: "글 쓰기"}}
                     />
                 </div>
-                <SearchBox
-                    handleChange={handleSearch}
-                    styles={{ marginBottom: 10 }}
-                />
                 <SearchResultBox
-                    items={searchList}
+                    items = {searchList}
                     handleClick={handlePost}
                 />
-                <div style={{
+                <div style = {{
                     display: "flex",
-                    justifyContent: "center",
+                    justifyContent:"center",
                     fontSize: 12
-                }}>
+                }}
+                >
                     1
                 </div>
             </div>
         </>
-    );
-};
-
-export default PostPage;
+    )
+}
+export default PostPage
