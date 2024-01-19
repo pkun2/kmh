914a4e2a06dbf54f9d2afcaab6bf43e20348b77d
minkinghub <kp141@naver.com>
2024-01-19 17:11:51 +0900

    login context save

--- src/main/frontend/src/services/index.js
+++ src/main/frontend/src/services/index.js
@@ -1,4 +1,5 @@
 import postData from "./postData";
 import getData from "./getData";
+import {useAuth, AuthProvider} from "./AuthProvider"
 
-export { postData, getData }
+export { postData, getData, useAuth, AuthProvider }
