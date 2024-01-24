69a38fa2aa086f2fc2722cbc657bf0a99f627460
minkinghub <kp141@naver.com>
2024-01-24 13:44:45 +0900

    postData

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -21,33 +21,34 @@ import {
 
 function App() {
   return (
-      <BrowserRouter>
-        <div className="home-container">
-          <Header/>
-        </div>
-        <div>
-          <Routes>
-            <Route path="/home" element={<HomePage/>}/>
-            <Route path="/write" element={<WritePage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/post" element={<PostPage />} />
-            <Route path="/postdetail" element={<PostDetailPage />} />
-            <Route path="/signup" element={<SignUpPage/>}/>
-            <Route path="/notice" element={<NoticePage/>}/>
-            <Route path="/profile" element={<ProfilePage/>}/>
-            <Route path="/search" element={<MainSearchPage/>}/>
-            <Route path="/trpg" element={<TrpgMain/>}/>
-            <Route path="/trpg/make" element={<TrpgMake/>}/>
-            <Route path="/apitest" element={<APITestPage/>}/>
-            <Route path="/channel" element={<ChannelPage/>
+        <BrowserRouter>
+          <div className="home-container">
+            <Header/>
+          </div>
+          <div>
+            <Routes>
+              <Route path="/home" element={<HomePage/>}/>
+              <Route path="/write" element={<WritePage/>}/>
+              <Route path="/login" element={<LoginPage/>}/>
+              <Route path="/post" element={<PostPage />} />
+              <Route path="/postdetail" element={<PostDetailPage />} />
+              <Route path="/signup" element={<SignUpPage/>}/>
+              <Route path="/notice" element={<NoticePage/>}/>
+              <Route path="/profile" element={<ProfilePage/>}/>
+              <Route path="/search" element={<MainSearchPage/>}/>
+              <Route path="/trpg" element={<TrpgMain/>}/>
+              <Route path="/trpg/make" element={<TrpgMake/>}/>
+              <Route path="/apitest" element={<APITestPage/>}/>
+              <Route path="/channel" element={<ChannelPage/>
 
-            }/>
-          </Routes>
-        </div>
-        <div>
-          <Footer/>
-        </div>
-      </BrowserRouter>);
+              }/>
+            </Routes>
+          </div>
+          <div>
+            <Footer/>
+          </div>
+        </BrowserRouter>
+      );
 }
 
 export default App;
