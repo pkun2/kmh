66508b148074ed8ede87c3264d31ed94cc64449c
pkun2 <pkun2@naver.com>
2024-01-30 13:11:41 +0900

    해당채널의 게시글을 확인하도록 설정

---
+++ src/main/java/com/website/kmh/dto/GetPostDto.java
@@ -0,0 +1,22 @@
+package com.website.kmh.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class GetPostDto {
+    private Long id;
+    private Long userId;
+    private String nickname;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private Integer viewCount;
+    private String categoryTag;
+    private Long channelId;
+    private String channelName;
+    private Integer goodCount;
+    private Integer badCount;
+}
